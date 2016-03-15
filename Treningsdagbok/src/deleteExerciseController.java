30ecec012b0d99b04a31c683c53234760e0a402c
Thomas Borresen <Thomas Borresen>
2016-03-15 12:26:40 +0100

    emty exerciseDelete

---
+++ Treningsdagbok/src/deleteExerciseController.java
@@ -0,0 +1,11 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+public class deleteExerciseController {
+	@FXML
+	private ComboBox cmbDelete;
+	
+	@FXML
+	private Button btnDelete;
+}
