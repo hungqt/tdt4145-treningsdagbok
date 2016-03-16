671c9e32307c42626ff383d8386b71d171870f2c
Thomas Borresen <Thomas Borresen>
2016-03-16 20:13:40 +0100

    final release

--- Treningsdagbok/src/AddExerciseController.java
+++ Treningsdagbok/src/AddExerciseController.java
@@ -85,7 +85,6 @@ public class AddExerciseController {
 		alert.setContentText("Session was added!");
 		alert.showAndWait();
 		main.showVelkommen();
-		main.showVelkommen();
 	}
 	
 }
--- Treningsdagbok/src/AddTrainingSessionController.java
+++ Treningsdagbok/src/AddTrainingSessionController.java
@@ -1,7 +1,9 @@
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
  
  public class AddTrainingSessionController {
 	
@@ -32,13 +34,24 @@ import javafx.scene.control.TextField;
  	
  	@FXML
  	public TrainingSession getTrainingSessionInfo(TrainingSession session){
+ 		
+ 		try{
+ 		
  		String dateString = dato.getValue().toString();
  		String startTime = tid.getText().toString();
  		String duration = varighet.getText().toString();
  		String shape = personligForm.getText().toString();
  		session = new TrainingSession(dateString, startTime, duration, shape);
- 		
  		return session;
+ 		}
+ 		catch(Exception e){
+ 			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error Dialog");
+			alert.setHeaderText("Error");
+			alert.setContentText("Invalid input time fields need to be integers!");
+			alert.showAndWait();
+ 		}
+ 		return null;
  	}
  	@FXML
  	public void cancel(){
@@ -49,6 +62,9 @@ import javafx.scene.control.TextField;
  	public void showAddExercises(){
  		System.out.println("test");
  		trainingSession = getTrainingSessionInfo(trainingSession);
- 		main.showAddExerciseToSession(trainingSession);
+ 		if(trainingSession != null){
+ 			main.showAddExerciseToSession(trainingSession);
+ 		}
+ 		
  	}
  }
